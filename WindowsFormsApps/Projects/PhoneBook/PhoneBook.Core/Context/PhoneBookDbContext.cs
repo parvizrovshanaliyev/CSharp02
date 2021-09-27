b3cd71f7062b41876dfd97c7176a74cc5ece11ad
Parviz Rovshan Aliyev <parvizrovshanaliyev@gmail.com>
2021-09-27 11:42:44 +0400

    create json files ready

--- WindowsFormsApps/Projects/PhoneBook/PhoneBook.Core/Context/PhoneBookDbContext.cs
+++ WindowsFormsApps/Projects/PhoneBook/PhoneBook.Core/Context/PhoneBookDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Text.Json;
 using System.Text.RegularExpressions;
 using Newtonsoft.Json;
 using PhoneBook.Entities;
@@ -11,17 +12,25 @@ namespace PhoneBook.Core.Context
 {
     public class PhoneBookDbContext
     {
-        private readonly string _path;
+       
+        #region .::ctor::.
         public PhoneBookDbContext()
         {
-            var appRoot = AppRoot();
-            _path = appRoot + "/" + "Context";
-            
-            ExistDatabase();
-            DeserializeObject();
+            _approot = AppRoot();
+            _path = Directory.GetParent(_approot)?.FullName + "/PhoneBook.Core/Context";
             _contacts ??= new List<Contact>();
             _users ??= new List<User>();
+            EnsureOrCreateDatabase();
+            DeserializeObject();
         }
+        #endregion
+        #region .::fields and properties::.
+        private readonly string _path;
+        private readonly string _approot;
+        private const string usersJson = "/Users.json";
+        private const string contactsJson = "/Contacts.json";
+
+
         private static List<Contact> _contacts;
         public List<Contact> Contacts
         {
@@ -30,14 +39,14 @@ namespace PhoneBook.Core.Context
         }
 
         private static List<User> _users;
-
         public List<User> Users
         {
             get => _users;
             set => _users = value;
         }
+        #endregion
 
-        private void ExistDatabase()
+        private void EnsureOrCreateDatabase()
         {
             /*
              * cari path-de qovlugun olub olmamasini yoxlayiriq 
@@ -49,57 +58,129 @@ namespace PhoneBook.Core.Context
             {
                 Directory.CreateDirectory(_path);
             }
-            else
+
+            var path = Directory.GetParent(_approot)?.FullName;
+            string coreDLL = $@"{path}\PhoneBook.Core\bin\Debug\net5.0\PhoneBook.Core.dll";
+
+            Assembly assembly = Assembly.LoadFile(coreDLL);
+
+            Type type = assembly.GetType("PhoneBook.Core.Context.PhoneBookDbContext");
+
+            if (type is not null)
             {
-                var user = new User
+                // get props
+                var propList = type.GetProperties().ToList();
+
+                propList.ForEach(i =>
                 {
-                    Username = "admin",
-                    Password = "admin123!"
-                };
-
-                var users = new List<User> { user };
-                // install nuget Newtonsoft.Json;
-                string userJson = JsonConvert.SerializeObject(users);
-                File.WriteAllText($"{_path}/users.json", userJson);
-
-                bool control = Exists($"{_path}/contacts.json");
-                if (!control) CreateFile($"{_path}/contacts.json");
+                    var filePath = $"{_path}/{i.Name.ToString()}.json";
+                    
+                    if (!Exists(filePath))
+                    {
+                        CreateFile(filePath);
+                        //SerializeObjToJson(_path + usersJson, users);
+                    }
+                });
             }
+
+            // new version
+            //DataSeeder();
+
+            // old version
+            // install nuget Newtonsoft.Json;
+            //string userJson = JsonConvert.SerializeObject(users);
+            //File.WriteAllText($"{_path}/users.json", userJson);
+
+            //bool control = Exists($"{_path}/contacts.json");
+            //if (!control) CreateFile($"{_path}/contacts.json");
         }
+
+        /// <summary>
+        /// Db yaradilarken default elave edilecek datalar insert edilir
+        /// </summary>
+        private void DataSeeder()
+        {
+            var user = new User
+            {
+                Username = "admin",
+                Password = "admin123!"
+            };
+
+            var users = new List<User> { user };
+            // new version
+            SerializeObjToJson(_path + usersJson, users);
+        }
+
         static void CreateFile(string path)
         {
             FileStream fileStream = File.Create(path);
             fileStream.Close();
         }
+
         static bool Exists(string path)
         {
             return File.Exists(path);
         }
+
         private void DeserializeObject()
         {
-            if (Exists($"{_path}/users.json"))
+            if (Exists($"{_path}{usersJson}"))
             {
-                string jsonData = File.ReadAllText(_path + "/users.json");
+                string jsonData = File.ReadAllText(_path + usersJson);
                 _users = JsonConvert.DeserializeObject<List<User>>(jsonData);
             }
 
-            if (Exists($"{_path}/contacts.json"))
+            if (Exists($"{_path}{contactsJson}"))
             {
-                string jsonData = File.ReadAllText(_path + "/contacts.json");
+                string jsonData = File.ReadAllText(_path + contactsJson);
                 _contacts = JsonConvert.DeserializeObject<List<Contact>>(jsonData);
             }
-
-            
         }
+
         public void SaveChanges()
         {
             if (Contacts.Any() && Contacts != null)
             {
-                string jsonData = JsonConvert.SerializeObject(_contacts);
-                File.WriteAllText(_path + "contacts.json",jsonData);
+                // new generic
+                SerializeObjToJson(_path + contactsJson, _contacts);
+                // old
+                //string jsonData = JsonConvert.SerializeObject(_contacts);
+                //File.WriteAllText(_path + "/contacts.json",jsonData);
             }
         }
 
+        /// <summary>
+        /// daxil edilen path-de T tipinde daxil edilen collection-nin json-a map edilmesi
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="path"></param>
+        /// <param name="data"></param>
+        private static void SerializeObjToJson<T>(string path, List<T> data)
+        {
+            // using System.Text.Json;
+            string json = System.Text.Json.JsonSerializer.Serialize(data, typeof(List<T>), new JsonSerializerOptions()
+            {
+                Encoder = System.Text.Encodings.Web.JavaScriptEncoder.Create(System.Text.Unicode.UnicodeRanges.All),
+                WriteIndented = true
+            });
+
+            AppendAllText(path, json);
+        }
+
+        /// <summary>
+        /// daxil edilen path-deki jsona append 
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="data"></param>
+        static void AppendAllText(string path, string data)
+        {
+            File.AppendAllText(path, data);
+        }
+
+        /// <summary>
+        /// set as startup project hansidirsa onun base path-ni verir
+        /// </summary>
+        /// <returns></returns>
         private static string AppRoot()
         {
             var exePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase);
--- WindowsFormsApps/Projects/PhoneBook/PhoneBook.UI.WinFormsApp/PhoneBookForm.Designer.cs
+++ WindowsFormsApps/Projects/PhoneBook/PhoneBook.UI.WinFormsApp/PhoneBookForm.Designer.cs
@@ -34,6 +34,7 @@ namespace PhoneBook.UI.WinFormsApp
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.textBoxAddress = new System.Windows.Forms.TextBox();
             this.label8 = new System.Windows.Forms.Label();
             this.textBoxNumber3 = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
@@ -50,10 +51,9 @@ namespace PhoneBook.UI.WinFormsApp
             this.textBoxName = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.textBoxAddress = new System.Windows.Forms.TextBox();
-            this.buttonCreate = new System.Windows.Forms.Button();
             this.textBoxDescription = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.buttonCreate = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.tabControl1.SuspendLayout();
@@ -67,8 +67,10 @@ namespace PhoneBook.UI.WinFormsApp
             this.groupBox1.Controls.Add(this.listBoxContact);
             this.groupBox1.Dock = System.Windows.Forms.DockStyle.Left;
             this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(284, 771);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Size = new System.Drawing.Size(248, 562);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Contacts";
@@ -77,19 +79,22 @@ namespace PhoneBook.UI.WinFormsApp
             // 
             this.listBoxContact.Dock = System.Windows.Forms.DockStyle.Fill;
             this.listBoxContact.FormattingEnabled = true;
-            this.listBoxContact.ItemHeight = 20;
-            this.listBoxContact.Location = new System.Drawing.Point(3, 23);
+            this.listBoxContact.ItemHeight = 15;
+            this.listBoxContact.Location = new System.Drawing.Point(3, 18);
+            this.listBoxContact.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.listBoxContact.Name = "listBoxContact";
-            this.listBoxContact.Size = new System.Drawing.Size(278, 745);
+            this.listBoxContact.Size = new System.Drawing.Size(242, 542);
             this.listBoxContact.TabIndex = 0;
             // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.tabControl1);
             this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBox2.Location = new System.Drawing.Point(284, 0);
+            this.groupBox2.Location = new System.Drawing.Point(248, 0);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(1081, 560);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox2.Size = new System.Drawing.Size(946, 404);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "groupBox2";
@@ -99,10 +104,11 @@ namespace PhoneBook.UI.WinFormsApp
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
             this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tabControl1.Location = new System.Drawing.Point(3, 23);
+            this.tabControl1.Location = new System.Drawing.Point(3, 18);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(1075, 534);
+            this.tabControl1.Size = new System.Drawing.Size(940, 384);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
@@ -123,192 +129,207 @@ namespace PhoneBook.UI.WinFormsApp
             this.tabPage1.Controls.Add(this.label2);
             this.tabPage1.Controls.Add(this.textBoxName);
             this.tabPage1.Controls.Add(this.label1);
-            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(1067, 501);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tabPage1.Size = new System.Drawing.Size(932, 356);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Contact";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
+            // textBoxAddress
+            // 
+            this.textBoxAddress.Location = new System.Drawing.Point(73, 226);
+            this.textBoxAddress.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBoxAddress.Multiline = true;
+            this.textBoxAddress.Name = "textBoxAddress";
+            this.textBoxAddress.Size = new System.Drawing.Size(840, 126);
+            this.textBoxAddress.TabIndex = 15;
+            // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(15, 311);
+            this.label8.Location = new System.Drawing.Point(13, 233);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(62, 20);
+            this.label8.Size = new System.Drawing.Size(49, 15);
             this.label8.TabIndex = 14;
             this.label8.Text = "Address\r\n";
             // 
             // textBoxNumber3
             // 
-            this.textBoxNumber3.Location = new System.Drawing.Point(688, 167);
+            this.textBoxNumber3.Location = new System.Drawing.Point(602, 125);
+            this.textBoxNumber3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBoxNumber3.Name = "textBoxNumber3";
-            this.textBoxNumber3.Size = new System.Drawing.Size(349, 27);
+            this.textBoxNumber3.Size = new System.Drawing.Size(306, 23);
             this.textBoxNumber3.TabIndex = 13;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(586, 174);
+            this.label7.Location = new System.Drawing.Point(513, 130);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(75, 20);
+            this.label7.Size = new System.Drawing.Size(60, 15);
             this.label7.TabIndex = 12;
             this.label7.Text = "Number 3\r\n";
             // 
             // textBoxNumber2
             // 
-            this.textBoxNumber2.Location = new System.Drawing.Point(688, 103);
+            this.textBoxNumber2.Location = new System.Drawing.Point(602, 77);
+            this.textBoxNumber2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBoxNumber2.Name = "textBoxNumber2";
-            this.textBoxNumber2.Size = new System.Drawing.Size(349, 27);
+            this.textBoxNumber2.Size = new System.Drawing.Size(306, 23);
             this.textBoxNumber2.TabIndex = 11;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(586, 110);
+            this.label6.Location = new System.Drawing.Point(513, 82);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(75, 20);
+            this.label6.Size = new System.Drawing.Size(60, 15);
             this.label6.TabIndex = 10;
             this.label6.Text = "Number 2";
             // 
             // textBoxNumber1
             // 
-            this.textBoxNumber1.Location = new System.Drawing.Point(688, 36);
+            this.textBoxNumber1.Location = new System.Drawing.Point(602, 27);
+            this.textBoxNumber1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBoxNumber1.Name = "textBoxNumber1";
-            this.textBoxNumber1.Size = new System.Drawing.Size(349, 27);
+            this.textBoxNumber1.Size = new System.Drawing.Size(306, 23);
             this.textBoxNumber1.TabIndex = 9;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(586, 43);
+            this.label5.Location = new System.Drawing.Point(513, 32);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(75, 20);
+            this.label5.Size = new System.Drawing.Size(60, 15);
             this.label5.TabIndex = 8;
             this.label5.Text = "Number 1\r\n";
             // 
             // textBoxWebSite
             // 
-            this.textBoxWebSite.Location = new System.Drawing.Point(83, 230);
+            this.textBoxWebSite.Location = new System.Drawing.Point(73, 172);
+            this.textBoxWebSite.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBoxWebSite.Name = "textBoxWebSite";
-            this.textBoxWebSite.Size = new System.Drawing.Size(349, 27);
+            this.textBoxWebSite.Size = new System.Drawing.Size(306, 23);
             this.textBoxWebSite.TabIndex = 7;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(15, 233);
+            this.label4.Location = new System.Drawing.Point(13, 175);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(62, 20);
+            this.label4.Size = new System.Drawing.Size(49, 15);
             this.label4.TabIndex = 6;
             this.label4.Text = "Website\r\n";
             // 
             // textBoxEmail
             // 
-            this.textBoxEmail.Location = new System.Drawing.Point(83, 171);
+            this.textBoxEmail.Location = new System.Drawing.Point(73, 128);
+            this.textBoxEmail.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBoxEmail.Name = "textBoxEmail";
-            this.textBoxEmail.Size = new System.Drawing.Size(349, 27);
+            this.textBoxEmail.Size = new System.Drawing.Size(306, 23);
             this.textBoxEmail.TabIndex = 5;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(31, 174);
+            this.label3.Location = new System.Drawing.Point(27, 130);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(46, 20);
+            this.label3.Size = new System.Drawing.Size(36, 15);
             this.label3.TabIndex = 4;
             this.label3.Text = "Email";
             // 
             // textBoxSurname
             // 
-            this.textBoxSurname.Location = new System.Drawing.Point(83, 107);
+            this.textBoxSurname.Location = new System.Drawing.Point(73, 80);
+            this.textBoxSurname.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBoxSurname.Name = "textBoxSurname";
-            this.textBoxSurname.Size = new System.Drawing.Size(349, 27);
+            this.textBoxSurname.Size = new System.Drawing.Size(306, 23);
             this.textBoxSurname.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(10, 110);
+            this.label2.Location = new System.Drawing.Point(9, 82);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(67, 20);
+            this.label2.Size = new System.Drawing.Size(54, 15);
             this.label2.TabIndex = 2;
             this.label2.Text = "Surname\r\n";
             // 
             // textBoxName
             // 
-            this.textBoxName.Location = new System.Drawing.Point(83, 40);
+            this.textBoxName.Location = new System.Drawing.Point(73, 30);
+            this.textBoxName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBoxName.Name = "textBoxName";
-            this.textBoxName.Size = new System.Drawing.Size(349, 27);
+            this.textBoxName.Size = new System.Drawing.Size(306, 23);
             this.textBoxName.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(28, 43);
+            this.label1.Location = new System.Drawing.Point(24, 32);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(49, 20);
+            this.label1.Size = new System.Drawing.Size(39, 15);
             this.label1.TabIndex = 0;
             this.label1.Text = "Name\r\n";
             // 
             // tabPage2
             // 
             this.tabPage2.Controls.Add(this.textBoxDescription);
-            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(1067, 501);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tabPage2.Size = new System.Drawing.Size(933, 376);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Description";
             this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // textBoxDescription
+            // 
+            this.textBoxDescription.Location = new System.Drawing.Point(13, 14);
+            this.textBoxDescription.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBoxDescription.Multiline = true;
+            this.textBoxDescription.Name = "textBoxDescription";
+            this.textBoxDescription.Size = new System.Drawing.Size(911, 352);
+            this.textBoxDescription.TabIndex = 0;
             // 
             // groupBox3
             // 
             this.groupBox3.Controls.Add(this.buttonCreate);
             this.groupBox3.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.groupBox3.Location = new System.Drawing.Point(284, 560);
+            this.groupBox3.Location = new System.Drawing.Point(248, 404);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(1081, 211);
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox3.Size = new System.Drawing.Size(946, 158);
             this.groupBox3.TabIndex = 1;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "groupBox3";
             // 
-            // textBoxAddress
-            // 
-            this.textBoxAddress.Location = new System.Drawing.Point(83, 311);
-            this.textBoxAddress.Multiline = true;
-            this.textBoxAddress.Name = "textBoxAddress";
-            this.textBoxAddress.Size = new System.Drawing.Size(960, 166);
-            this.textBoxAddress.TabIndex = 15;
-            // 
             // buttonCreate
             // 
             this.buttonCreate.Dock = System.Windows.Forms.DockStyle.Top;
-            this.buttonCreate.Location = new System.Drawing.Point(3, 23);
+            this.buttonCreate.Location = new System.Drawing.Point(3, 18);
+            this.buttonCreate.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.buttonCreate.Name = "buttonCreate";
-            this.buttonCreate.Size = new System.Drawing.Size(1075, 29);
+            this.buttonCreate.Size = new System.Drawing.Size(940, 22);
             this.buttonCreate.TabIndex = 16;
             this.buttonCreate.Text = "New Contact";
             this.buttonCreate.UseVisualStyleBackColor = true;
             this.buttonCreate.Click += new System.EventHandler(this.buttonCreate_Click);
             // 
-            // textBoxDescription
-            // 
-            this.textBoxDescription.Location = new System.Drawing.Point(15, 18);
-            this.textBoxDescription.Multiline = true;
-            this.textBoxDescription.Name = "textBoxDescription";
-            this.textBoxDescription.Size = new System.Drawing.Size(1041, 468);
-            this.textBoxDescription.TabIndex = 0;
-            // 
             // PhoneBookForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1365, 771);
+            this.ClientSize = new System.Drawing.Size(1194, 562);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "PhoneBookForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "PhoneBookForm";

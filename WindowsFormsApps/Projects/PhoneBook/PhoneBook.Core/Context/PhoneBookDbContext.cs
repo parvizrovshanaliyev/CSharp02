f5a2e347eb3c790bb5b2a795bc03aea5d772ddab
Parviz Rovshan Aliyev <parvizrovshanaliyev@gmail.com>
2021-10-03 21:39:27 +0400

    ....

--- WindowsFormsApps/Projects/PhoneBook/PhoneBook.Core/Context/PhoneBookDbContext.cs
+++ WindowsFormsApps/Projects/PhoneBook/PhoneBook.Core/Context/PhoneBookDbContext.cs
@@ -1,39 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Dynamic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Text.Encodings.Web;
 using System.Text.Json;
 using System.Text.RegularExpressions;
+using System.Text.Unicode;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
 using PhoneBook.Entities;
+using JsonSerializer = System.Text.Json.JsonSerializer;
 
 namespace PhoneBook.Core.Context
 {
     public class PhoneBookDbContext
     {
-       
         #region .::ctor::.
+
         public PhoneBookDbContext()
         {
             _approot = AppRoot();
             _path = Directory.GetParent(_approot)?.FullName + "/PhoneBook.Core/Context";
             EnsureOrCreateDatabase();
-            //DeserializeObject();
+            // eger db yaradildiqdan sonra hec bir data olmazsa new instance-lar yaradilir.
             _contacts ??= new List<Contact>();
             _users ??= new List<User>();
         }
+
         #endregion
         #region .::fields and properties::.
+
         private readonly string _path;
         private readonly string _approot;
         private const string usersJson = "/Users.json";
         private const string contactsJson = "/Contacts.json";
 
-
         private static List<Contact> _contacts;
+
         public List<Contact> Contacts
         {
             get => _contacts;
@@ -41,33 +43,39 @@ namespace PhoneBook.Core.Context
         }
 
         private static List<User> _users;
+
         public List<User> Users
         {
             get => _users;
             set => _users = value;
         }
+
         #endregion
 
+        /// <summary>
+        /// Database evez edecek json file-larin create edilmesi,
+        /// Default olaraq evvecelden daxil edilmeli olan datalarin insert edilmesi 
+        /// </summary>
         private void EnsureOrCreateDatabase()
         {
             /*
              * cari path-de qovlugun olub olmamasini yoxlayiriq 
              */
-            
-            bool existDir=Directory.Exists(_path);
 
-            if (!existDir)
-            {
-                Directory.CreateDirectory(_path);
-            }
+            var existDir = Directory.Exists(_path);
 
+            if (!existDir) Directory.CreateDirectory(_path);
+            /*
+             * Core dll icerisinde PhoneBookDbContext classinin entity-lere
+             * uygun gelen collection type olan proplarina uygun json file yaradilir.
+             * Bunlar Db-daki cedvellerimizi evez edecek.
+             */
             var path = Directory.GetParent(_approot)?.FullName;
-            string coreDLL = $@"{path}\PhoneBook.Core\bin\Debug\net5.0\PhoneBook.Core.dll";
+            var coreDLL = $@"{path}\PhoneBook.Core\bin\Debug\net5.0\PhoneBook.Core.dll";
 
-            Assembly assembly = Assembly.LoadFile(coreDLL);
+            var assembly = Assembly.LoadFile(coreDLL);
+            var type = assembly.GetType("PhoneBook.Core.Context.PhoneBookDbContext");
 
-            Type type = assembly.GetType("PhoneBook.Core.Context.PhoneBookDbContext");
-            
             if (type is not null)
             {
                 // get props
@@ -75,12 +83,10 @@ namespace PhoneBook.Core.Context
 
                 propList.ForEach(i =>
                 {
+                    // prop adina uygun json fileName
                     var filePath = $"{_path}/{i.Name.ToString()}.json";
-                    
-                    if (!Exists(filePath))
-                    {
-                        CreateFile(filePath);
-                    }
+
+                    if (!Exists(filePath)) CreateFile(filePath);
                 });
             }
 
@@ -97,66 +103,69 @@ namespace PhoneBook.Core.Context
         }
 
         /// <summary>
-        /// Db yaradilarken default elave edilecek datalar insert edilir
+        ///     Db yaradilarken default elave edilecek datalar insert edilir
         /// </summary>
         private void DataSeeder()
         {
+            // Json data-nin Collectiona map edilmesi.
             DeserializeObject();
+
             if (_users == null)
             {
-                var user = new User
-                {
-                    Username = "admin",
-                    Password = "admin123!"
-                };
+                var user = new User { Username = "admin", Password = "admin123!" };
 
                 var users = new List<User> { user };
                 // new version
                 SerializeObjToJson(_path + usersJson, users);
             }
-           
         }
 
-        static void CreateFile(string path)
+        private static void CreateFile(string path)
         {
-            FileStream fileStream = File.Create(path);
+            var fileStream = File.Create(path);
             fileStream.Close();
         }
 
-        static bool Exists(string path)
+        private static bool Exists(string path)
         {
             return File.Exists(path);
         }
 
+        /// <summary>
+        ///     Json data-nin Collectiona map edilmesi.
+        /// </summary>
         private void DeserializeObject()
         {
             if (Exists($"{_path}{usersJson}"))
             {
-                string jsonData = File.ReadAllText(_path + usersJson);
+                var jsonData = File.ReadAllText(_path + usersJson);
                 _users = JsonConvert.DeserializeObject<List<User>>(jsonData);
             }
 
             if (Exists($"{_path}{contactsJson}"))
             {
-                string jsonData = File.ReadAllText(_path + contactsJson);
+                var jsonData = File.ReadAllText(_path + contactsJson);
                 _contacts = JsonConvert.DeserializeObject<List<Contact>>(jsonData);
             }
         }
 
+        /// <summary>
+        /// Collection-larda olan deyiskliklerin yeniden json-a yazilmasi
+        /// </summary>
         public void SaveChanges()
         {
             if (Contacts.Any() && Contacts != null)
-            {
                 // new generic
                 SerializeObjToJson(_path + contactsJson, _contacts);
-                // old
-                //string jsonData = JsonConvert.SerializeObject(_contacts);
-                //File.WriteAllText(_path + "/contacts.json",jsonData);
-            }
+
+            
+            // old
+            //string jsonData = JsonConvert.SerializeObject(_contacts);
+            //File.WriteAllText(_path + "/contacts.json",jsonData);
         }
 
         /// <summary>
-        /// daxil edilen path-de T tipinde daxil edilen collection-nin json-a map edilmesi
+        ///     daxil edilen path-de T tipinde daxil edilen collection-nin json-a map edilmesi
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="path"></param>
@@ -164,36 +173,39 @@ namespace PhoneBook.Core.Context
         private static void SerializeObjToJson<T>(string path, List<T> data)
         {
             // using System.Text.Json;
-            string json = System.Text.Json.JsonSerializer.Serialize(data, typeof(List<T>), new JsonSerializerOptions()
-            {
-                Encoder = System.Text.Encodings.Web.JavaScriptEncoder.Create(System.Text.Unicode.UnicodeRanges.All),
-                WriteIndented = true
-            });
+            var json = JsonSerializer.Serialize(data, typeof(List<T>),
+                new JsonSerializerOptions
+                {
+                    Encoder = JavaScriptEncoder.Create(UnicodeRanges
+                        .All),
+                    WriteIndented = true
+                });
 
             WriteAllText(path, json);
         }
 
         /// <summary>
-        /// daxil edilen path-deki jsona append 
+        ///     daxil edilen path-deki jsona append
         /// </summary>
         /// <param name="path"></param>
         /// <param name="data"></param>
-        static void WriteAllText(string path, string data)
+        private static void WriteAllText(string path, string data)
         {
             File.WriteAllText(path, data);
         }
 
         /// <summary>
-        /// set as startup project hansidirsa onun base path-ni verir
+        ///     set as startup project hansidirsa onun base path-ni verir
         /// </summary>
         /// <returns></returns>
         private static string AppRoot()
         {
             var exePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase);
-            Regex appPathMatcher = new Regex(@"(?<!fil)[A-Za-z]:\\+[\S\s]*?(?=\\+bin)");
-            var appRoot = appPathMatcher
-                .Match(exePath ?? string.Empty).Value;
+            var appPathMatcher = new Regex(@"(?<!fil)[A-Za-z]:\\+[\S\s]*?(?=\\+bin)");
+            var appRoot = appPathMatcher.Match(exePath ?? string.Empty).Value;
             return appRoot;
         }
+
+       
     }
 }
